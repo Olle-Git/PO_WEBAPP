d1f3749324c847e782baf49db8b7cec6cf9b0105
Adrian Bleeker <204091@stmichaelcollege.nl>
2024-11-15 16:03:47 +0100

    --no-edit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
     <center><img src="img/NYP.png" max-width="830px"></center>
     <div class="flex-container">
-        <a href="#login"><div class="box"><img src="img/icon.png" width="100px" class="icon"><br>Klant</div></a>
+        <a href="index.php?klant=true#login" onclick="isKlant()"><div class="box"><img src="img/icon.png" width="100px" class="icon"><br>Klant</div></a>
         <a href="#login"><div class="box"><img src="img/icon.png" width="100px" class="icon"><br>Medewerker</div></a>
         <a href="#login"><div class="box"><img src="img/icon.png" width="100px" class="icon"><br>Bezorger</div></a>
     </div>
@@ -21,12 +21,41 @@
     <center>
     <div class="flex-container-form" style="margin-top: 200px;" id="login">
     <h1 style="font-size: xx-large;">Login</h1><br>
-    <Form method="POST">
-        <input type="text" placeholder="username"><br>
-        <input type="password" placeholder="passsword"><br>
+    <Form action="formhandler.php" method="POST">
+        <input type="text" placeholder="username" name="username"><br>
+        <input type="password" placeholder="passsword" name="password"><br>
+        <input type="submit" value="Login">
         <input type="button" value="Login">
     </Form>
     </div>
     </center>
+<?php
+$username = $wachtwoord = null;
+function isKlant() {
+    $isklant = True;
+    echo "klant";
+}
+function isMedewerker() {
+    $isMedewerker = True;
+    echo 'medewerker';
+}
+function isBezorger() {
+    $isBezorger = True;
+    echo 'bezorger';
+}
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+if ($_POST['username'] ?? null && $_POST['wachtwoord'] ?? null) {
+    $username = test_input($_POST['username']);
+    $wachtwoord = test_input($_POST['wachtwoord']);
+    $hashedwachtwoord = password_hash($wachtwoord, PASSWORD_DEFAULT);
+    submit($username, $hashedwachtwoord, $wachtwoord);
+}
+
+?>
 </body>
 </html>
